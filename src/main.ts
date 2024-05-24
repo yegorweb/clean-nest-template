fe864dc9eefefe7fbef4592cc9ed00f0b30efdb8
Yegor Knyazev <egorknyazev10@gmail.com>
2024-05-24 15:26:04 +0400

    change main, app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors({ 
+    origin: [process.env.CLIENT_URL, 'http://localhost:3001'],
+    credentials: true
+  })
+  app.useGlobalFilters(new HttpExceptionFilter())
+
+  app.use(cookieParser())
+
+  await app.listen(process.env.PORT)
 }
-bootstrap();
+bootstrap()
