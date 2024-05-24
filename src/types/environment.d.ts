01818684a365e29040d0f5e085ec3d848c0225d2
Yegor Knyazev <egorknyazev10@gmail.com>
2024-05-24 15:29:59 +0400

    add env interface

---
+++ src/types/environment.d.ts
@@ -0,0 +1,23 @@
+export {}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string
+      HTTPS: string
+      
+      MONGO_URL: string
+      CLIENT_URL: string
+      
+      JWT_ACCESS_SECRET: string
+      JWT_REFRESH_SECRET: string
+      
+      EMAIL: string
+      EMAIL_PASSWORD: string    
+
+      YC_KEY_ID: string
+      YC_SECRET: string
+      YC_BUCKET_NAME: string
+    }
+  }
+}
