eea1a5f50cdbfbd36a3180de43cdbb20f75b7e22
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-04 20:18:33 +0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  let originFromEnv: string[] = []
+  try {
+    originFromEnv = JSON.parse(process.env.CLIENT_URLS)
+  } catch (error) {
+    console.error('Ошибка парсинга переменной окружения CLIENT_URLS:')
+    console.error(error)
+  }
+  app.enableCors({
+    origin: [...originFromEnv, ],
+    credentials: true
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
