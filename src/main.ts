478885a1c8fe6c0c6cf77ce2e1acc0adcbc2c18e
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-05 12:37:29 +0400

    app use cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
     origin: [...originFromEnv, ],
     credentials: true
   })
+
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
